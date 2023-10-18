ec2545e6b0e3dd8447f184b0eaf6453efe33667b
Ivan Kurzenev <vakin213@gmail.com>
2023-10-19 00:37:11 +0300

    bind the service to the controller

--- backend/src/tasks/tasks.service.ts
+++ backend/src/tasks/tasks.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private readonly tasks = [];
+
+  async getAllTasks() {
+    return this.tasks;
+  }
+}
