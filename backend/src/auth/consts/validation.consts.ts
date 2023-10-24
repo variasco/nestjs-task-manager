7ca83d909031daba8a3128e0e8f5457e1d57e66e
Ivan Kurzenev <vakin213@gmail.com>
2023-10-24 23:22:42 +0300

    add validation to sign up method

---
+++ backend/src/auth/consts/validation.consts.ts
@@ -0,0 +1,13 @@
+import { ValidationArguments } from 'class-validator';
+
+export const PASSWOR_TOO_WEAK =
+  'Пароль должен содержать хотя бы 1 заглавную букву, 1 строчную букву, 1 цифру или специальный символ';
+
+export const IS_A_STRING = ({ property }: ValidationArguments) =>
+  `Поле ${property} должно быть строкой`;
+
+export const MAX_LENGTH = ({ property, constraints }: ValidationArguments) =>
+  `Поле ${property} не может быть больше ${constraints} символов`;
+
+export const MIN_LENGTH = ({ property, constraints }: ValidationArguments) =>
+  `Поле ${property} не может быть меньше ${constraints} символов`;
--- backend/src/auth/dto/auth-credential.dto.ts
+++ backend/src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,15 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+import { IS_A_STRING, MAX_LENGTH, MIN_LENGTH, PASSWOR_TOO_WEAK } from '../consts/validation.consts';
+
 export class AuthCredentialsDto {
+  @IsString({ message: IS_A_STRING })
+  @MinLength(4, { message: MIN_LENGTH })
+  @MaxLength(20, { message: MAX_LENGTH })
   username: string;
+
+  @IsString({ message: IS_A_STRING })
+  @MinLength(8, { message: MIN_LENGTH })
+  @MaxLength(20, { message: MAX_LENGTH })
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, { message: PASSWOR_TOO_WEAK })
   password: string;
 }
