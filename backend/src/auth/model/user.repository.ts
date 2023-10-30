d519217635c83737f00dc63fc78a05a560314609
Ivan Kurzenev <vakin213@gmail.com>
2023-10-31 00:03:14 +0300

    implement salt generation and password hashing

--- backend/src/auth/model/user.repository.ts
+++ backend/src/auth/model/user.repository.ts
@@ -3,6 +3,7 @@ import { DataSource, Repository } from 'typeorm';
 import { USER_ALREADY_EXIST } from '../consts/validation.consts';
 import { AuthCredentialsDto } from '../dto/auth-credential.dto';
 import { User } from './user.entity';
+import { genSalt, hash } from 'bcrypt';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
@@ -13,9 +14,13 @@ export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
+    const salt = await genSalt();
+    const hashedPassword = await this.hashPassword(password, salt);
+
     const user = this.create({
+      salt,
       username,
-      password,
+      password: hashedPassword,
     });
 
     try {
@@ -27,5 +32,9 @@ export class UserRepository extends Repository<User> {
 
       throw new InternalServerErrorException('Server error');
     }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return hash(password, salt);
   }
 }
