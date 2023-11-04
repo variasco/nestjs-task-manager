e67e8454b96d16a0760aceaa40288097d81613e3
Ivan Kurzenev <vakin213@gmail.com>
2023-11-05 00:50:32 +0300

    enhanced GetUser decorator

--- backend/src/auth/decorators/get-user.decorator.ts
+++ backend/src/auth/decorators/get-user.decorator.ts
@@ -1,8 +1,11 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+import { User } from '../model/user.entity';
 
-const GetUser = createParamDecorator((_: unknown, input: ExecutionContext) => {
-  const request = input.switchToHttp().getRequest();
-  return request.user;
+const GetUser = createParamDecorator<keyof User, ExecutionContext>((userKey, ctx) => {
+  const request = ctx.switchToHttp().getRequest();
+  const user = request.user as User;
+
+  return userKey ? user?.[userKey] : user;
 });
 
 export default GetUser;
