701789187414188135c3f2fd0e5e8a285f690dfb
Ivan Kurzenev <vakin213@gmail.com>
2023-11-05 14:40:58 +0300

    add a user guard to the tasks routes

--- backend/src/tasks/tasks.module.ts
+++ backend/src/tasks/tasks.module.ts
@@ -4,9 +4,10 @@ import { Task } from './model/task.entity';
 import { TaskRepository } from './model/task.repository';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Task]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService, TaskRepository],
 })
